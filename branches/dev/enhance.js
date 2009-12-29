f2893885cd92d61001bbc87679306c467a0e6bfc
scott@scottjehl.com <scott@scottjehl.com@a4b61afa-c981-11de-b743-054a4f195298>
2009-12-29 15:14:24 +0000

    added new dev branch including rewrite by Brandon Aaron

---
+++ branches/dev/enhance.js
@@ -0,0 +1,351 @@
+/*
+ * enhance.js - Test-Driven Progressive Enhancement
+ * Authors: Scott Jehl (filamentgroup.com), Brandon Aaron (brandonaaron.net)
+ * Dual licensed under the MIT (filamentgroup.com/examples/mit-license.txt) and GPL (filamentgroup.com/examples/gpl-license.txt) licenses.
+*/
+(function(win, doc) {
+    
+var settings, body,
+    head = doc.getElementsByTagName('head')[0] || doc.documentElement;
+
+enhance = function(options) {
+    options  = options || {};
+    settings = {};
+    
+    // mixin settings
+    for (var name in enhance.defaultSettings) {
+        var option = options[name];
+        settings[name] = option !== undefined ? option : enhance.defaultSettings[name];
+    }
+    
+    // mixin additional tests
+    for (var test in options.addTests) {
+        settings.tests[test] = options.addTests[test];
+    }
+    
+    runTests();
+    
+    if (settings.applyDocReadyHack) {
+        applyDocReadyHack();
+    }
+    
+    if (settings.appendToggleLink) {
+        windowLoad(function() { 
+            appendToggleLinks();
+        });
+    }
+};
+
+enhance.defaultTests = {
+    getById: function() {
+        return !!doc.getElementById;
+    },
+    getByTagName: function() {
+        return !!doc.getElementsByTagName;
+    },
+    createEl: function() {
+        return !!doc.createElement;
+    },
+    boxmodel: function() {
+        var newDiv = doc.createElement('div');
+        newDiv.style.cssText = 'width: 1px; padding: 1px;';
+        body.appendChild(newDiv);
+        var divWidth = newDiv.offsetWidth;
+        body.removeChild(newDiv);
+        return divWidth === 3;
+    },
+    position: function() {
+        var newDiv = doc.createElement('div');
+        newDiv.style.cssText = 'position: absolute; left: 10px;';
+        body.appendChild(newDiv);
+        var divLeft = newDiv.offsetLeft;
+        body.removeChild(newDiv);
+        return divLeft === 10;
+    },
+    'float': function() {
+        var newDiv = doc.createElement('div'),
+            style = 'style="width: 5px; float: left;"';
+        newDiv.innerHTML = '<div ' + style + '></div><div ' + style + '></div>';
+        body.appendChild(newDiv);
+        var childNodes = newDiv.childNodes,
+            divTopA = childNodes[0].offsetTop,
+            divTopB = childNodes[1].offsetTop;
+        body.removeChild(newDiv);
+        return divTopA === divTopB;
+    },
+    clear: function() {
+        var newDiv = doc.createElement('div'),
+            style = 'width: 5px; height: 5px; float: left;';
+        newDiv.innerHTML = '<div style="' + style + '"></div><div style="' + style + ' clear: left;"></div>';
+        body.appendChild(newDiv);
+        var childNodes = newDiv.childNodes,
+            divTopA = childNodes[0].offsetTop,
+            divTopB = childNodes[1].offsetTop;
+        body.removeChild(newDiv);
+        return divTopA !== divTopB;
+    },
+    overflow: function() {
+        var newDiv = doc.createElement('div');
+        newDiv.innerHTML = '<div style="height: 10px; overflow: hidden;"></div>';
+        body.appendChild(newDiv);
+        var divHeight = newDiv.offsetHeight;
+        body.removeChild(newDiv);
+        return divHeight === 10;
+    },
+    ajax: function() {
+        //factory test borrowed from quirksmode.org
+        var xmlhttp = false, index = -1, factory,
+            XMLHttpFactories = [
+                function() { return new XMLHttpRequest() },
+                function() { return new ActiveXObject("Msxml2.XMLHTTP") },
+                function() { return new ActiveXObject("Msxml3.XMLHTTP") },
+                function() { return new ActiveXObject("Microsoft.XMLHTTP") }
+            ];
+        while ((factory = XMLHttpFactories[++index])) {
+            try { xmlhttp = factory(); }
+            catch (e) { continue; }
+            break;
+        }
+        return !!xmlhttp;
+    },
+    resize: function() {
+        return win.onresize != false;
+    },
+    print: function() {
+        return !!win.print;
+    }
+};
+
+enhance.defaultSettings = {
+    testName: 'enhanced',
+    loadScripts: [],
+    loadStyles: [],
+    queueLoading: true,
+    appendToggleLink: true,
+    forcePassText: 'View High-bandwidth version',
+    forceFailText: 'View Low-bandwidth version',
+    tests: enhance.defaultTests,
+    applyDocReadyHack: true,
+    addTests: {},
+    log: false
+};
+
+function forceFail() {
+    createCookie(settings.testName, 'fail');
+    win.location.reload();
+}
+enhance.forceFail = forceFail;
+
+function forcePass() {
+    createCookie(settings.testName, 'pass');
+    win.location.reload();
+}
+enhance.forcePass = forcePass;
+
+function reTest() {
+    eraseCookie(settings.testName);
+    win.location.reload();
+}
+enhance.reTest = reTest;
+
+function runTests() {
+    var result = readCookie(settings.testName);
+        
+    //check for cookies from a previous test
+    if (result) {
+        if (result === 'pass') {
+            enhancePage();
+        }
+    }
+    //no cookies - run tests
+    else {
+        bodyOnReady(function() {
+            var pass = true;
+            for (var name in settings.tests) {
+                pass = settings.tests[name]();
+                if (!pass) {
+                    if (settings.log) {
+                        alert(name + ' failed');
+                    }
+                    break;
+                }
+            }
+            
+            result = pass ? 'pass' : 'fail';
+            createCookie(settings.testName, result);
+            if (pass) {
+                enhancePage();
+            }
+        });
+    }
+}
+
+function bodyOnReady(callback) {
+    var checkBody = setInterval(bodyReady, 1);
+    function bodyReady() {
+        if (doc.body) {
+            body = doc.body;
+            clearInterval(checkBody);
+            callback();
+        }
+    }
+}
+
+function windowLoad(callback) {
+    var oldonload = win.onload
+    win.onload = function() {
+        if (oldonload) { oldonload(); }
+        callback();
+    }
+}
+
+function appendToggleLinks() {
+    if (!settings.appendToggleLink) { return; }
+    
+    var checkCookie = readCookie(settings.testName);
+        
+    if (checkCookie) {
+        var a = doc.createElement('a');
+        a.href = "#";
+        a.tabindex = 0;
+        a.className = settings.testName + '_toggleResult';
+        a.innerHTML = checkCookie === 'pass' ? settings.forceFailText : settings.forcePassText;
+        a.onclick   = checkCookie === 'pass' ? enhance.forceFail : enhance.forcePass;
+        doc.getElementsByTagName('body')[0].appendChild(a);
+    }
+}
+
+function enhancePage() {
+    if (doc.documentElement.className.indexOf(settings.testName) === -1) {
+        doc.documentElement.className += ' ' + settings.testName;
+    }
+    
+    if (settings.loadStyles.length) {
+        appendStyles();
+    }
+    if (settings.loadScripts.length) {
+        settings.queueLoading ? appendScriptsSync() : appendScriptAsync();
+    }
+}
+
+function appendStyles() {
+    var index = -1,
+        name;
+    
+    while ((name = settings.loadStyles[++index])) {
+        var link  = doc.createElement('link');
+        
+        link.type = 'text/css';
+        link.rel  = 'stylesheet';
+        
+        if (typeof name === 'string') {
+            link.href = name;
+            head.appendChild(link);
+        }
+        else {
+            for (var attr in name) {
+                link.setAttribute(attr, name[attr]);
+            }
+            if (attr === 'iecondition' && (/MSIE (\d+)\.\d+;/).test(navigator.userAgent)) {
+                var ieversion = new Number(RegExp.$1);
+                if (name['iecondition'] === 'all' || name['iecondition'] === ieversion) {
+                    head.appendChild(link); 
+                }
+            }
+            else {
+                head.appendChild(link);
+            }
+        }
+    }
+}
+
+function appendScriptsSync() {
+    var queue = [].concat(settings.loadScripts);
+    
+    function next() {
+        if (queue.length === 0) {
+            return;
+        }
+        
+        var src    = queue.shift();
+            script = createScriptTag(src),
+            done   = false;
+        
+        script.onload = script.onreadystatechange = function() {
+            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                done = true;
+                next();
+                this.onload = this.onreadystatechange = null;
+            }
+        }
+        head.insertBefore(script, head.firstChild);
+    }
+    
+    next();
+}
+
+function appendScriptsAsync() {
+    var index = -1,
+        name;
+        
+    while ((name = settings.loadScripts[++index])) {
+        head.insertBefore(createScriptTag(name), head.firstChild);
+    }
+}
+
+function createScriptTag(src) {
+    var script  = doc.createElement('script');
+    script.type = 'text/javascript';
+    script.src  = src;
+    return script;
+}
+
+/*cookie functions from quirksmode.org*/
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    doc.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = doc.cookie.split(';');
+    for (var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+
+function applyDocReadyHack() {
+    // via http://webreflection.blogspot.com/2009/11/195-chars-to-help-lazy-loading.html
+    // verify that document.readyState is undefined
+    // verify that document.addEventListener is there
+    // these two conditions are basically telling us
+    // we are using Firefox < 3.6
+    if (doc.readyState == null && doc.addEventListener){
+        // on DOMContentLoaded event, supported since ages
+        doc.addEventListener("DOMContentLoaded", function DOMContentLoaded(){
+            // remove the listener itself
+            doc.removeEventListener("DOMContentLoaded", DOMContentLoaded, false);
+            // assign readyState as complete
+            doc.readyState = "complete";
+        }, false);
+        // set readyState = loading or interactive
+        // it does not really matter for this purpose
+        doc.readyState = "loading";
+    }
+}
+
+
+})(window, document);
