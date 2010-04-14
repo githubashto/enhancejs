e22733deab6566263424ae3c3b5e0a9ff6dcab54
scott@scottjehl.com <scott@scottjehl.com@a4b61afa-c981-11de-b743-054a4f195298>
2010-04-14 19:22:39 +0000

    our cookies are good for 90 days. mmmmm cookies...

--- enhance.js
+++ enhance.js
@@ -416,14 +416,12 @@ function createScriptTag(item) {
     }
 }
 
-/*cookie functions from quirksmode.org*/
+/* cookie functions from quirksmode.org (modified) */
 function createCookie(name, value, days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
+    days = days || 90;
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
     doc.cookie = name+"="+value+expires+"; path=/";
 }
 
