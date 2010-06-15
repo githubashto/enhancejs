e200c350ac6ede4a4dd303f030e36d2b61c2424f
scottjehl <scottjehl@a4b61afa-c981-11de-b743-054a4f195298>
2010-06-15 02:31:01 +0000

    modified unit tests a little to include a test for basic css loading, basic js loading.

--- branches/unit/tests/unit-tests/unit/unit.js
+++ branches/unit/tests/unit-tests/unit/unit.js
@@ -6,8 +6,8 @@ testWithIframe('iframe test helper', function(win, doc, enhance) {
     ok(enhance, 'enhance reference');
 });
 
-testWithIframe('pass and basics', function(win, doc, enhance) {
-    expect(7);
+testWithIframe('pass and basics (cookies required for full test)', function(win, doc, enhance) {
+    expect(9);
     
     enhance.defaultTests = {
         fail: function() {
@@ -30,24 +30,27 @@ testWithIframe('pass and basics', function(win, doc, enhance) {
                 return true;
             }
         },
-        loadScripts: ['../data/pass.js', '../data/onErrorTest-falseurl.js'],
-        loadStyles: ['pass.css'],
+        loadStyles: ['../data/pass.css'],
+        loadScripts: ['../data/pass.js'],
         onPass: function() {
             start();
+            ok(enhance.cookiesSupported, 'Cookies are available');
             var pass = readCookie(settings.testName);
             equals(readCookie(enhance.defaultSettings.testName), null, 'default testName was not used');
             ok(pass != null, 'testName setting properly used');
             equals(pass, 'pass', 'onPass successfully called after passing tests');
             ok(true, 'onPass successfully called');
         },
-        onFail: function() {
-            start();
-            ok(false, 'onFail was called');
-        },
-        onLoadError: function() {
-            ok(true, 'onLoadError was called after script failed to load');
+        onScriptsLoaded: function(){
+        	ok(win.passJSLoaded, 'onScriptsLoaded was called after scripts finished loading');
+        	//css should be loaded at this point too, so we can try testing it
+        	var testDivA = doc.body.appendChild(doc.createElement('div'));
+    		testDivA.id = 'testDivA';
+   			 ok(testDivA.offsetWidth == 10, 'CSS files were successfully loaded');
         }
     };
+    
+    
     
     eraseCookie(settings.testName);
     eraseCookie(enhance.defaultSettings.testName);
